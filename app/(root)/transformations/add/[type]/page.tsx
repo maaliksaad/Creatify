9422f2d636ab3730ff087dd21779a3b3a465d115
maaliksaad <2021-bs-cs-154@tuf.edu.pk>
2024-04-03 08:31:56 +0500

    fix webhook connection

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,32 @@
+import Header from "@/components/shared/header";
+import TransformationForm from "@/components/shared/transformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { SearchParamProps, TransformationTypeKey } from "@/types/index";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const AddTranformationType = () => {
-  return <div>AddTranformationType</div>;
+const AddTranformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  const tranformations = transformationTypes[type];
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  return (
+    <>
+      <Header title={tranformations.title} subTitle={tranformations.subTitle} />
+      <TransformationForm
+        action="Add"
+        userId={user._Id}
+        type={tranformations.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
+  );
 };
 
-export default AddTranformationType;
+export default AddTranformationTypePage;
