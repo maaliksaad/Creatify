00f860ef1ca3d28428b1b88d1ff1609751d4b922
maaliksaad <2021-bs-cs-154@tuf.edu.pk>
2024-04-05 02:34:34 +0500

    fix: clerk callback issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    WEBHOOK_SECRET: process.env.WEBHOOK_SECRET,
+  },
+};
 
 export default nextConfig;
